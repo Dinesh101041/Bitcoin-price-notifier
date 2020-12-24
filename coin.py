207b22c0e5e804b43777f18e68e8dd6ae9f1215e
Dinesh101041 <dinesh101041@gmail.com>
2020-12-24 14:33:51 +0530

    final change

--- coin.py
+++ coin.py
@@ -2,26 +2,34 @@ import requests
 import time
 from datetime import datetime
 
-BITCOIN_API_URL = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+BITCOIN_PRICE_THRESHOLD = 10000
+BITCOIN_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '1d311920-4c90-4f9d-93cd-00c35a4e274c',
+}
 IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/test_event/with/key/jwVYNxBeSGdJCGueBnirO7cJOxn_fUn2G_XNPvArUJh'
 
 def get_latest_bitcoin_price():
     response = requests.get(BITCOIN_API_URL)
     response_json = response.json()
-    # Convert the price to a floating point number
-    return float(response_json[0]['price_usd'])
-
+    return response_json  # Convert the price to a floating point number
 
 def post_ifttt_webhook(event, value):
-    # The payload that will be sent to IFTTT service
-    data = {'value1': value}
-    # inserts our desired event
-    ifttt_event_url = IFTTT_WEBHOOKS_URL.format(event)
-    # Sends a HTTP POST request to the webhook URL
-    requests.post(ifttt_event_url, json=data)
+    data = {'value1': value}  # The payload that will be sent to IFTTT service
+    ifttt_event_url = IFTTT_WEBHOOKS_URL.format(event)  # Inserts our desired event
+    requests.post(ifttt_event_url, json=data)  # Sends a HTTP POST request to the webhook URL
 
-BITCOIN_PRICE_THRESHOLD = 10000    
+def format_bitcoin_history(bitcoin_history):
+    rows = []
+    for bitcoin_price in bitcoin_history:
+        date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M')  # Formats the date into a string: '24.02.2018 15:09'
+        price = bitcoin_price['price']
+        # <b> (bold) tag creates bolded text
+        row = '{}: $<b>{}</b>'.format(date, price)  # 24.02.2018 15:09: $<b>10123.4</b>
+        rows.append(row)
 
+    return '<br>'.join(rows)  # Join the rows delimited by <br> tag: row1<br>row2<br>row3
 
 def main():
     bitcoin_history = []
@@ -30,20 +38,14 @@ def main():
         date = datetime.now()
         bitcoin_history.append({'date': date, 'price': price})
 
-        # Send an emergency notification
-        if price < BITCOIN_PRICE_THRESHOLD:
-            post_ifttt_webhook('bitcoin_price_emergency', price)
-
+        
         # Send a Telegram notification
-        # Once we have 5 items in our bitcoin_history send an update
-        if len(bitcoin_history) == 5:
+        if len(bitcoin_history) == 5:  # Once we have 5 items in our bitcoin_history send an update
             post_ifttt_webhook('bitcoin_price_update', format_bitcoin_history(bitcoin_history))
-            # Reset the history
+           
             bitcoin_history = []
 
-        # Sleep for 5 minutes 
-        # (For testing purposes you can set it to a lower number)
-        time.sleep(5 * 60)
+        
 
 if __name__ == '__main__':
     main()
